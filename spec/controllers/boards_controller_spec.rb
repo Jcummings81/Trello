1d96e9d6d7f9431dba63cf23ffa8b536198397a5
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-09-11 17:51:25 -0600

    the latest

--- spec/controllers/boards_controller_spec.rb
+++ spec/controllers/boards_controller_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe BoardsController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
-    end
+        end
   end
 
   describe "GET #show" do
